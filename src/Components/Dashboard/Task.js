b66ecb8eb4dddc0b8f2472ca57da5c45143adfbe
Vasavi K <vasavikasireddi@gmail.com>
2024-10-18 15:41:05 +0530

    Icons added

--- src/Components/Dashboard/Task.js
+++ src/Components/Dashboard/Task.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./dashboard.css";
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck, faCircleExclamation, faClock } from "@fortawesome/free-solid-svg-icons";
 
 const Task = ({ task, onFormat, onViewTask }) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
@@ -26,6 +28,9 @@ const Task = ({ task, onFormat, onViewTask }) => {
       <div {...attributes} {...listeners} className="task-drag-handle">
         <p className="title">{task.title}</p>
         <p>{task.description}</p>
+        <p>Status : {task.status === "done" ? 
+          <FontAwesomeIcon icon={faCircleCheck} style={{ fontSize: "1.5em", color: "green"}} /> 
+          : task.status === "in-progress" ? <FontAwesomeIcon icon={faClock} style={{ fontSize: "1.5em", color: "#ff7900" }} /> : <FontAwesomeIcon icon={faCircleExclamation} style={{ fontSize: "1.5em", color: "007fff" }} />}</p>
         <p className="time">Created at : {onFormat(task.date)}</p>
       </div>
       <div className="btns">
